11ee1aab27db5e395cb9766d79463175a496b111
frontend-huyu <yu.huang0206@gmail.com>
2022-04-21 17:54:20 +0800

    Update Two Sum solutions

---
+++ 1_two_sum.js
@@ -0,0 +1,28 @@
+// solution1 RT: 144ms Memo: 42.4MB
+var twoSum = function (nums, target) {
+  let result = []
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        result[0] = i
+        result[1] = j
+      }
+    }
+  }
+  return result
+};
+
+// solution2 RT: 93ms Memo: 42.7MB
+var twoSum = function (nums, target) {
+  let result = {}
+  for (let i = 0; i < nums.length; i++) {
+    let remainder = target - nums[i]
+
+    if (remainder in result) {
+      return [result[remainder], i]
+    }
+
+    result[nums[i]] = i
+  }
+  return null
+};
